8e68d5213333df9d3cd42972fb299ebb1c61f5c1
Samuka-pixel <shark06778@sharkcoders.pt>
2024-11-09 10:08:43 +0000

    Add files via upload

---
+++ categoria_atletas.py
@@ -0,0 +1,7 @@
+I = int(input("Quantos anos tem o atleta?: "))
+if I <= 13:
+    print("Equipa Mercurio")
+elif I <= 16:
+    print("Equipa Neptuno")
+else:
+    print("Equipa Jupiter")
---
+++ marcas_carro.py
@@ -0,0 +1,20 @@
+Car = str(input("Qual é a marca do carro?: "))
+if Car == "toyota":
+    print("Asia")
+elif Car == "nissan":
+    print("Asia")
+elif Car == "Opel":
+    print("Europa")
+elif Car == "Volkwagen":
+    print("Europa")
+elif Car == "Ford":
+    print("America")
+elif Car == "Birkin":
+    print("Africa")
+elif Car == "Harper":
+    print("Africa")
+elif Car == "Jeep":
+    print("America")
+else:
+    print("Essa marca não é valida :(")
+
